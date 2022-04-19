190849631d2cddff7666f695312c222cfd811b90
stanila hermione mantho tadzon <stanila-hermione.tadzon-mantho@informatik.hs-fulda.de>
2022-04-19 17:24:55 +0200

    init

---
+++ ArrayManipulation.java
@@ -0,0 +1,10 @@
+public class ArrayManipulation{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        System.out.println(ReverseArray(arr));
+    }
+    public static int[] ReverseArray(int[] arr, int startpos){
+        // base
+        
+    }
+}
